afa08a4235158e2873f441a4ffbeefdee68eeeec
madiciunca-ship-it <madiciunca@gmail.com>
2025-11-07 03:17:07 +0200

    git add pages/files git commit -m "files details" git push

--- src/data/books.js
+++ src/data/books.js
@@ -85,7 +85,7 @@ export const BOOKS = [
     coverUrl: "/assets/books/gallery/o-zi-de-care-sa-ti-amintesti-cover.JPEG",
     extraImage: "/assets/books/gallery/o-zi-de-care-sa-ti-amintesti-back.JPEG",
 
-    sampleUrl: "/files/sample-o-zi-de-care-sa-ti-amintesti-ro",
+    sampleUrl: "/files/sample-o-zi-de-care-sa-ti-amintesti-ro.pdf",
 
     tags: ["eBook", "PDF", "EPUB", "travel"],
     format: "eBook",
@@ -131,7 +131,7 @@ export const BOOKS = [
     coverUrl: "/assets/books/zile-si-nopti-de-vietnam-cover.png",
     extraImage: "/assets/books/zile-si-nopti-de-vietnam-spate.png",
 
-    sampleUrl: "/files/sample-zile-si-nopti-de-vietnam-bucati-dintr-un-suflet-nomad-ro",
+    sampleUrl: "/files/sample-zile-si-nopti-de-vietnam-bucati-dintr-un-suflet-nomad-ro.pdf",
 
     tags: ["memorii", "călătorii", "cultură", "travel", "autentic"],
     format: "eBook",
